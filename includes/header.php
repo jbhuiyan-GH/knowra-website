bb98541fbf6d091a2796f6e3a8c8b8547e41b12c
Md. Jahiruddin Bhuiyan <jbhuiyan@gmail.com>
2026-06-16 12:21:44 +0600

    Bust Cloudflare CSS cache with version query

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600;700;800&family=Newsreader:ital,opsz,wght@0,6..72,400;0,6..72,500;1,6..72,400&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="/knowra.css">
+<link rel="stylesheet" href="/knowra.css?v=2">
 </head>
 <body>
 <div class="page">
